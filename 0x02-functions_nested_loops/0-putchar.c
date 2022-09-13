41054d39ecb8ad2f4e350cb0367bf5d308da5a02
Monsuru Bello <111158967+Monsuru775@users.noreply.github.com>
2022-09-13 22:26:26 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
-#include "main.h"
+#include "holberton.h"
 /**
- * main - Print "_putchar" followed by a new line.
+ * main - Print "Holberton" followed by a new line.
  * Description: You are not allowed to include standard libraries.
  * Return: 0
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n')
-		
-	return(0);
+	char main[] = "main";
+	int i = 0;
+
+	while (main[i] != '\0')
+	{
+		_putchar(main[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
 }
