966b13976a775d120b978f0408fbc997ec5bfb29
Monsuru Bello <111158967+Monsuru775@users.noreply.github.com>
2022-09-17 02:13:01 +0100

    Create 5-main.c

---
+++ 0x04-more_functions_nested_loops/5-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    more_numbers();
+    return (0);
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Pri
