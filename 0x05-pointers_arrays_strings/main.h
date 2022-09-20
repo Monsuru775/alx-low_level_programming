c5691a1a854d9b2bd773e7b7abecb98df0ccbd4b
Monsuru Bello <111158967+Monsuru775@users.noreply.github.com>
2022-09-20 04:46:39 +0100

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef   MAIN_H
+#define MAINN_H
+
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -9,9 +12,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
+int _atoi(char *s);
+
+#endif /* HOLBERTON_H */
